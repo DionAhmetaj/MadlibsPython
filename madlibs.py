92e54efd44bd2defc4c36b37b346eed042c2969d
DionAhmetaj <97095386+DionAhmetaj@users.noreply.github.com>
2022-01-04 15:08:50 -0800

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,49 @@
+
+time_of_day = input(" Time of day:")
+adjective1 = input(" Adjective: ")
+kind_of_shoes = input(" Kind of Shoes:")
+body_part = input(" Body part: ")
+plural_noun1 = input(" Plural noun: ")
+running_partner  = input(" Running partner: ")
+verb1 = input("verb:")
+plural_noun2 = input("plural noun:")
+adjective2 = input('adjective:')
+adjective3 = input("adjective:")
+verb_ending_in_ING = input(' Verb ending in ING:')
+animal = input(' Animal:')
+exclamation = input('exclamation:')
+emotion = input("emotion:")
+adjective4 = input('adjective:')
+adjective5 = input('adjective:')
+past_tense_verb1 = input('past tense verb:')
+verb2 = input('verb:')
+past_tense_verb2 = input("past tense verb:")
+adjective6 = input('adjective:')
+favorite_nutrition = input('favorite nutrition food:')
+adjective7 = input('adjective:')
+activity = input('activity:')
+adjective8 = input('adjective:')
+
+madlibs = ' We set out at ' + time_of_day + " and headed for the " + adjective1 + ' trails in the Vuthaj Mountains. '\
+    + " When we arrived,we laced up our " + kind_of_shoes + " and stretched our " + body_part + '.'\
+        + ' we forgot our ' + plural_noun1 + " but " + running_partner + " said it was too late to turn back. "\
+            + ' The fall leaves were just beginning to ' + verb1 + " from the " + plural_noun2 + ' and made a ' + adjective2\
+             + " sound underfoot." + ' it was the perfect day for a ' + adjective3 + " autumn run! "\
+                 + ' So,we started ' + verb_ending_in_ING + " and came upon a/an " + animal + '.'\
+                     + " it was the last thing we expected and " + running_partner + exclamation + '.'\
+                         + " We were so " + emotion + ' ! ' + running_partner + ' said that we needed to stand very ' + adjective4 + " and make the "\
+                             + animal + " feel " + adjective5 + '.' + " The " + animal + past_tense_verb1 + ' and we were able to ' + verb2 + ' safely away ' + '.'\
+                                 + " The rest of the day,we " + past_tense_verb2 + ' around the ' + adjective6 + " trails and ate " + favorite_nutrition\
+                                     + '.' + ' Overall,it was a ' + adjective7 + ' day to go fo a/an ' + activity + ' in Bellinghams ' + adjective8 + ' trail system! '
+
+print(madlibs)                                
+                                         
+
+
+ 
+
+
+
+
+
+
